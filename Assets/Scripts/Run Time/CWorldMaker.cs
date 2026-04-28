a189162b0d1e9e0abdff9dc75a646e45a63bb05a
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-04-28 17:49:29 +0900

    Update CWorldMaker.cs
    세포 자동자를 통한 구역 변경 추가.
    개선 필요함.

--- Assets/Scripts/Run Time/CWorldMaker.cs
+++ Assets/Scripts/Run Time/CWorldMaker.cs
@@ -31,6 +31,12 @@ public enum EAreaType
     Shortcut,
     Boss,
 }
+public enum ETileType
+{
+    Wall,
+    Floor,
+    Hole
+}
 
 public class CWorldMaker : MonoBehaviour
 {
@@ -41,10 +47,11 @@ public class CWorldMaker : MonoBehaviour
         public TreeNode _parentTree;
         // 트리 노드의 Rect
         public RectInt _nodeRect;
-        // 실제 생성된 방의 Rect
-        public RectInt _roomRect;
+        // 실제 생성된 방의 기준 Rect
+        public RectInt _standardRoomRect;
+        // 자신을 포함한 노드의 리스트
         public List<TreeNode> _nodeList = new List<TreeNode>();
-
+        public bool isHorizontal = false;
         public TreeNode(int x, int y, int width, int height)
         {
             // 기준점은 좌하단
@@ -67,27 +74,33 @@ public class CWorldMaker : MonoBehaviour
     [SerializeField] private float _roomMinRatio = 0.5f;
 
     [Header("Cellular Automata")]
+    [SerializeField] private float _wallRatio = 0.05f;
+    [SerializeField] private float _floorRatio = 0.5f;
+    [SerializeField] private float _holeRatio = 0.01f;
+    [SerializeField] private int _smoothRatio = 4;
+    [SerializeField] private int _smoothCount = 3;
 
     [Header("디버그 키")]
     [SerializeField] private KeyCode _makeData = KeyCode.M;
     #endregion
 
     #region 내부 변수
-    private int[,] _tileMap;
+    private ETileType[,] _tileMap;
+    private ETileType[,] _bufferTileMap;
     #endregion
 
     #region 유니티 이벤트
     void Awake()
     {
         // 타일맵 초기화
-        _tileMap = new int[_mapSize.x, _mapSize.y];
+        _tileMap = new ETileType[_mapSize.x, _mapSize.y];
+        _bufferTileMap = new ETileType[_mapSize.x, _mapSize.y];
 
         // 타일맵 데이터 생성.
         TreeNode rootNode = new TreeNode(0, 0, _mapSize.x, _mapSize.y);
         DivideTree(rootNode, 0);
         GenerateRoom(rootNode, 0);
         GenerateRoad(rootNode, 0);
-
         // Cellular Automata - SmoothMap
 
     }
@@ -102,7 +115,8 @@ public class CWorldMaker : MonoBehaviour
         if (Input.GetKeyDown(_makeData))
         {
             // 타일맵 초기화
-            _tileMap = new int[_mapSize.x, _mapSize.y];
+            _tileMap = new ETileType[_mapSize.x, _mapSize.y];
+            _bufferTileMap = new ETileType[_mapSize.x, _mapSize.y];
 
             // 타일맵 데이터 생성.
             TreeNode rootNode = new TreeNode(0, 0, _mapSize.x, _mapSize.y); //루트가 될 트리 생성
@@ -120,7 +134,8 @@ public class CWorldMaker : MonoBehaviour
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    mapData += _tileMap[i, j] == 0 ? "■" : "□";
+                    ETileType tile = _tileMap[i, j];
+                    mapData += tile == ETileType.Wall ? "■" : tile == ETileType.Floor ? "□" : "※";
                 }
                 mapData += "\n"; // 한 줄 끝날 때마다 줄바꿈
             }
@@ -150,7 +165,8 @@ public class CWorldMaker : MonoBehaviour
             int splitLength = Mathf.RoundToInt(Random.Range(longLength * (0.5f - _divideRatio), longLength * (0.5f + _divideRatio)));
 
             // 노드를 반으로 자른다.
-            if (size.width >= size.height)
+            node.isHorizontal = size.width >= size.height;
+            if (node.isHorizontal)
             {
                 node._leftTree = new TreeNode(size.x, size.y, splitLength, size.height);
                 node._rightTree = new TreeNode(size.x + splitLength, size.y, size.width - splitLength, size.height);
@@ -172,8 +188,11 @@ public class CWorldMaker : MonoBehaviour
         }
     }
 
-    private RectInt GenerateRoom(TreeNode node, int loopCount)
+    private void GenerateRoom(TreeNode node, int loopCount)
     {
+        // 자신 추가
+        node._nodeList.Add(node);
+
         if (loopCount == _maxLoopCount)
         {
             RectInt size = node._nodeRect;
@@ -184,26 +203,81 @@ public class CWorldMaker : MonoBehaviour
             int x = node._nodeRect.x + Random.Range(0, size.width - width);
             int y = node._nodeRect.y + Random.Range(0, size.height - height);
 
-            //
+            // 타일맵 반영
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-                    _tileMap[x + i, y + j] = 1;
+                    float total = _wallRatio + _floorRatio + _holeRatio;
+                    float flag = Random.Range(0f, total);
+                    //_tileMap[x + i, y + j] = flag < _floorRatio ? ETileType.Floor : ETileType.Wall;
+                    _tileMap[x + i, y + j] = flag < _floorRatio ? ETileType.Floor : flag < _floorRatio + _holeRatio ? ETileType.Hole : ETileType.Wall;
+                    _bufferTileMap[x + i, y + j] = _tileMap[x + i, y + j];
+                }
+            }
+            // smooth
+            // 버퍼에 작업
+            for (int k = 0; k < _smoothCount; k++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        _bufferTileMap[x + i, y + j] = CellularAutomata(x + i, y + j);
+                    }
+                }
+                // 적용
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        _tileMap[x + i, y + j] = _bufferTileMap[x + i, y + j];
+                    }
                 }
             }
 
-            return new RectInt(x, y, width, height);
+            node._standardRoomRect = new RectInt(x, y, width, height);
         }
-        loopCount++;
-        node._leftTree._roomRect = GenerateRoom(node._leftTree, loopCount);
-        node._rightTree._roomRect = GenerateRoom(node._rightTree, loopCount);
+        else
+        {
+            loopCount++;
+            GenerateRoom(node._leftTree, loopCount);
+            GenerateRoom(node._rightTree, loopCount);
 
-        // 자식들 중 중심에 더 가까운 방이 기준이다.
-        float lDist = Vector2.SqrMagnitude(node._leftTree._roomRect.center - node._nodeRect.center);
-        float rDist = Vector2.SqrMagnitude(node._rightTree._roomRect.center - node._nodeRect.center);
+            // 자식 추가.
+            List<TreeNode> lList = node._leftTree._nodeList;
+            List<TreeNode> rList = node._rightTree._nodeList;
 
-        return lDist > rDist ? node._rightTree._roomRect : node._leftTree._roomRect;
+            node._nodeList.AddRange(lList);
+            node._nodeList.AddRange(rList);
+
+            // 양쪽 자식들 중 제일 가까운 한 쌍이 기준이다.
+            int n = node._leftTree._nodeList.Count;
+            int m = node._rightTree._nodeList.Count;
+
+            float minSqrDist = float.MaxValue;
+
+            RectInt lRoom;
+            RectInt rRoom;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    lRoom = lList[i]._standardRoomRect;
+                    rRoom = rList[j]._standardRoomRect;
+
+                    float sqrDist = Vector2.SqrMagnitude(lRoom.center - rRoom.center);
+
+                    if (sqrDist < minSqrDist)
+                    {
+                        minSqrDist = sqrDist;
+                        node._leftTree._standardRoomRect = lRoom;
+                        node._rightTree._standardRoomRect = rRoom;
+                    }
+                }
+            }
+        }
     }
 
     private void GenerateRoad(TreeNode node, int loopCount)
@@ -211,16 +285,31 @@ public class CWorldMaker : MonoBehaviour
         if (loopCount == _maxLoopCount) return;
 
         // 자식 노드 2개의 _roomRect.center를 이어준다.
-        RectInt lRect = node._leftTree._roomRect;
-        RectInt rRect = node._rightTree._roomRect;
+        RectInt lRect = node._leftTree._standardRoomRect;
+        RectInt rRect = node._rightTree._standardRoomRect;
 
-        if (Mathf.Abs(lRect.center.x - rRect.center.x) > Mathf.Abs(lRect.center.y - rRect.center.y))
+        // 기준?
+        Vector2 lRectCenter = lRect.center;
+        Vector2 rRectCenter = rRect.center;
+        Vector2 center = (lRectCenter + rRectCenter) / 2;
+
+        if (node.isHorizontal)
         {
-            // 가로
+            // 연결하기.
+            if (rRectCenter.x < lRectCenter.x)
+            {
+                //for (int i = rRectCenter.x) ;
+            }
+            else
+            {
+
+            }
+
         }
         else
         {
-            // 세로
+            // 연결하기.
+
         }
     }
 
@@ -232,6 +321,62 @@ public class CWorldMaker : MonoBehaviour
     private void CreateTile()
     {
 
+    }
+
+    private ETileType CellularAutomata(int x, int y)
+    {
+        int wallCount = 0;
+        int floorCount = 0;
+        int holeCount = 0;
+        int endCount = 0;
+        // 현재 좌표를 기준으로 주변 8칸 검사
+        for (int nX = x - 1; nX <= x + 1; nX++)
+        {
+            for (int nY = y - 1; nY <= y + 1; nY++)
+            {
+                // 맵 범위 검사
+                if (nX >= 0 && nX < _mapSize.x && nY >= 0 && nY < _mapSize.y)
+                {
+                    if (nX == x && nY == y) continue;
+
+                    switch (_tileMap[nX, nY])
+                    {
+                        case ETileType.Wall: wallCount++; break;
+                        case ETileType.Floor: floorCount++; break;
+                        case ETileType.Hole: holeCount++; break;
+                    }
+                }
+                else
+                {
+                    // 맵 밖은 벽이다. 벽인가? 아닐 경우 여기를 수정.
+                    endCount++;
+                }
+            }
+        }
+        switch (_tileMap[x, y])
+        {
+            case ETileType.Wall:
+                // 벽은 바닥이 될 수 있다.
+                if (wallCount + endCount < _smoothRatio)
+                    return ETileType.Floor;
+
+                return ETileType.Wall;
+            case ETileType.Floor:
+                // 바닥은 구멍이 될 수 있다.
+                if (holeCount + wallCount > _smoothRatio)
+                    return ETileType.Hole;
+
+                // 바닥은 벽이 될 수 있다.
+                if (wallCount + endCount > _smoothRatio)
+                    return ETileType.Wall;
+
+                return ETileType.Floor;
+            case ETileType.Hole:
+
+                return ETileType.Hole;
+        }
+
+        return ETileType.Floor;
     }
     #endregion
 }
