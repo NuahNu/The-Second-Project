0f4c784860fe72144f15b683489e8f255ff0bfc7
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-05-06 17:29:53 +0900

    내비메쉬 테스트 중

--- Assets/Scripts/Run Time/CWorldMakerTester.cs
+++ Assets/Scripts/Run Time/CWorldMakerTester.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using NavMeshPlus.Components;
+using System.Collections;
 
 
 #region CWorldMaker
@@ -25,28 +27,39 @@ public class CWorldMakerTester : MonoBehaviour
 
     [Header("타일맵 관련")]
     [SerializeField] private Grid _grid;
-    // 바닥 타일
+    // 바닥 타일맵
     // 제일 나중에 그려진다. 
     [SerializeField] private Tilemap _tilemap;
     [SerializeField] private TilemapRenderer _tilemapRenderer;
 
-    // 구조물 타일
+    // 구조물 타일맵
     // 플레이어와 같은 Order Layer에 있어야 한다.
 
-    // 콜라이더 타일
+    // 콜라이더 타일맵
     // 얘들은 그려지면 안되지
+
+    // 네비메쉬 타일맵
+    // 화면에 그려지지는 않지만 네비메쉬 생성에 사용할 타일맵
 
     [Header("타일맵 리소스")]
     [SerializeField] private TileBase _tileBase;
 
+    [Header("네비메쉬")]
+    [SerializeField] private NavMeshSurface _surfase2D;
+
     [Header("디버그 키")]
-    [SerializeField] private KeyCode _makeData = KeyCode.M;
+    [SerializeField] private bool _showMapLog = false;
+
+    [SerializeField] private KeyCode _makeDataKey = KeyCode.M;
+    [SerializeField] private KeyCode _bakeKey = KeyCode.B;
     #endregion
 
     #region 내부 변수
     private ETileType[,] _tileTypeArray;
 
     private CWorldMaker _worldMaker;
+
+    //private
     #endregion
 
     #region 유니티 이벤트
@@ -54,6 +67,9 @@ public class CWorldMakerTester : MonoBehaviour
     {
         _worldMaker = new CWorldMaker();
         _tileTypeArray = _worldMaker.MakeWorld(_worldData, _BSPData, _CAData);
+
+        if (_surfase2D.IsNull("_surfase2D")) return;
+
         // 스폰 위치 지정.
     }
 
@@ -64,37 +80,18 @@ public class CWorldMakerTester : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(_makeData))
+        if (Input.GetKeyDown(_makeDataKey))
         {
-            _tileTypeArray = _worldMaker.MakeWorld(_worldData, _BSPData, _CAData);
-
-            int rows = _tileTypeArray.GetLength(0);
-            int cols = _tileTypeArray.GetLength(1);
-            string mapData = $"TileMap 데이터: \n";
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols; j++)
-                {
-                    ETileType tile = _tileTypeArray[i, j];
-                    mapData += tile.HasFlag(ETileType.Wall) ? "■" : tile.HasFlag(ETileType.Floor) ? "□" : "※";
-                }
-                mapData += "\n"; // 한 줄 끝날 때마다 줄바꿈
-            }
-
-            Debug.Log(mapData);
-
-            SetTile();
+            StartCoroutine(Co_MakeMap());
         }
     }
     #endregion
 
     #region public
-
     #endregion
 
     #region private
-    public void SetTile()
+    private void SetTile()
     {
         _tilemap.ClearAllTiles();
         // 얘를 반복 돌면서 읽어와 타일을 깐다.
@@ -122,6 +119,38 @@ public class CWorldMakerTester : MonoBehaviour
                 }
             }
         }
+    }
+
+    private IEnumerator Co_MakeMap()
+    {
+        _tileTypeArray = _worldMaker.MakeWorld(_worldData, _BSPData, _CAData);
+
+        if (_showMapLog)
+        {
+            int rows = _tileTypeArray.GetLength(0);
+            int cols = _tileTypeArray.GetLength(1);
+            string mapData = $"TileMap 데이터: \n";
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    ETileType tile = _tileTypeArray[i, j];
+                    mapData += tile.HasFlag(ETileType.Wall) ? "■" : tile.HasFlag(ETileType.Floor) ? "□" : "※";
+                }
+                mapData += "\n"; // 한 줄 끝날 때마다 줄바꿈
+            }
+
+            Debug.Log(mapData);
+        }
+
+        //Physics2D.SyncTransforms();
+
+        SetTile();
+        yield return null;
+        _surfase2D.BuildNavMesh();
+        //_surfase2D.UpdateNavMesh(_surfase2D.navMeshData);
+        //NavMeshBuilder.UpdateNavMeshDataAsync(); // ???
     }
     #endregion
 }
