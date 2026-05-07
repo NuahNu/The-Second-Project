948b02c033f6cac5fc52bf274b713a3775c7fb20
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-05-07 11:34:07 +0900

    유틸리티

---
+++ Assets/Scripts/Run Time/Define.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+
+#region Define
+/*
+
+*/
+#endregion
+
+public static class Define
+{
+    // Order in Layer
+    public static int ORDER_FLOOR = 1;
+    public static int ORDER_SHADOW = 1;
+    public static int ORDER_CHARACTER = 1;
+    public static int ORDER_STRUCTURE = 1;
+    public static int ORDER_COLLIDER = 1;
+    public static int ORDER_NAVMESH = 1;
+
+    // Tilemap name
+    public static string NAME_FLOOR = "Floor Tilemap";
+    public static string NAME_STRUCTURE = "Structure Tilemap";
+    public static string NAME_COLLIDER = "Collider Tilemap";
+    public static string NAME_NAVMESH = "Navmesh Tilemap";
+}
