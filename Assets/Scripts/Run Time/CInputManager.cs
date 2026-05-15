498c20da33f49a774807fc726444e2921181aa7b
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-05-15 14:50:23 +0900

    Create CInputManager.cs
    중앙 관리가 필요할 경우 사용

---
+++ Assets/Scripts/Run Time/CInputManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+
+#region CInputManager
+/*
+
+*/
+#endregion
+
+public class CInputManager : MonoBehaviour
+{
+    #region 인스펙터
+    
+    #endregion
+
+    #region 내부 변수
+    
+    #endregion
+
+    #region 유니티 이벤트
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region public
+    
+    #endregion
+
+    #region protected
+    
+    #endregion
+
+    #region private
+    
+    #endregion
+}
