6a24f32a835ca3d204690c0017a6558d0f182696
NuahNu <104414203+NuahNu@users.noreply.github.com>
2026-05-26 18:27:28 +0900

    임시 카메라 모드 Follow 추가

--- Assets/Scripts/Run Time/CCamera.cs
+++ Assets/Scripts/Run Time/CCamera.cs
@@ -10,9 +10,15 @@ using UnityEngine.InputSystem;
 
 public class CCamera : MonoBehaviour
 {
+    public enum ECameraMode
+    {
+        Follow,
+        Dynamic
+    }
+
     #region 인스펙터
     // 이것도 비율로?
-    [SerializeField]private CTileMapMaker _mapMaker;
+    [SerializeField] private CTileMapMaker _mapMaker;
     [Header("카메라 이동")]
     [SerializeField] private int _edgeSize = 10;
     [SerializeField] private float _cameraSpeedRatio = 1f;
@@ -22,6 +28,9 @@ public class CCamera : MonoBehaviour
     [SerializeField] private float _scrollRatio = 0.01f;
     [SerializeField] private Vector2 _scrollClamp = new Vector2(1, 10);
     [SerializeField] private bool _scrollFlip = true;
+
+    [Header("임시?")]
+    [SerializeField] private Transform _player;
     #endregion
 
     #region 내부 변수
@@ -29,6 +38,8 @@ public class CCamera : MonoBehaviour
 
 
     private Rect _gridRect;
+
+    private ECameraMode _cameraMode;
     #endregion
 
     #region 유니티 이벤트
@@ -36,6 +47,9 @@ public class CCamera : MonoBehaviour
     {
         _camera = Camera.main;
         if (_mapMaker.IsNull("_mapMaker")) return;
+        if (_player.IsNull("_player")) return;
+
+        _cameraMode = ECameraMode.Follow;
     }
 
     void Start()
@@ -55,54 +69,62 @@ public class CCamera : MonoBehaviour
 
     void Update()
     {
-
-        Vector2 mousePos = Mouse.current.position.ReadValue();
-
-        float screenWidth = Screen.width;
-        float screenHeight = Screen.height;
-
-        float cameraSpeed = _camera.orthographicSize * _cameraSpeedRatio;
-        //Debug.Log($"{cameraSpeed} = {_camera.orthographicSize} * {_cameraSpeedRatio}");
-
-        Vector3 delta = new Vector3();
-
-        if (mousePos.x < screenWidth - _edgeSize)
-            delta.x -= cameraSpeed * Time.deltaTime;
-
-        if (mousePos.x > _edgeSize)
-            delta.x += cameraSpeed * Time.deltaTime;
-
-        if (mousePos.y < screenHeight - _edgeSize)
-            delta.y -= cameraSpeed * Time.deltaTime;
-
-        if (mousePos.y > _edgeSize)
-            delta.y += cameraSpeed * Time.deltaTime;
-
-        _camera.transform.position += delta;
-        // clamp
-        // 클램프 값을 함수로 뽑으면 되는거 아니냐?
-        // 그러기 위해 클램프를 2번 해야할듯 하다.
-        if (_useClamp)
+        switch (_cameraMode)
         {
-            float x = Mathf.Clamp(_camera.transform.position.x, -_gridRect.width / 2, _gridRect.width / 2);
-            float y = Mathf.Clamp(_camera.transform.position.y, -_gridRect.height / 2, _gridRect.height / 2);
+            case ECameraMode.Follow:
+                transform.position = new Vector3(_player.position.x, _player.position.y, Define.CAMERA_Z);
+                break;
+            case ECameraMode.Dynamic:
+                Vector2 mousePos = Mouse.current.position.ReadValue();
 
-            // 2차 클램프
+                float screenWidth = Screen.width;
+                float screenHeight = Screen.height;
+
+                float cameraSpeed = _camera.orthographicSize * _cameraSpeedRatio;
+                //Debug.Log($"{cameraSpeed} = {_camera.orthographicSize} * {_cameraSpeedRatio}");
+
+                Vector3 delta = new Vector3();
+
+                if (mousePos.x < screenWidth - _edgeSize)
+                    delta.x -= cameraSpeed * Time.deltaTime;
+
+                if (mousePos.x > _edgeSize)
+                    delta.x += cameraSpeed * Time.deltaTime;
+
+                if (mousePos.y < screenHeight - _edgeSize)
+                    delta.y -= cameraSpeed * Time.deltaTime;
+
+                if (mousePos.y > _edgeSize)
+                    delta.y += cameraSpeed * Time.deltaTime;
+
+                _camera.transform.position += delta;
+                // clamp
+                // 클램프 값을 함수로 뽑으면 되는거 아니냐?
+                // 그러기 위해 클램프를 2번 해야할듯 하다.
+                if (_useClamp)
+                {
+                    float x = Mathf.Clamp(_camera.transform.position.x, -_gridRect.width / 2, _gridRect.width / 2);
+                    float y = Mathf.Clamp(_camera.transform.position.y, -_gridRect.height / 2, _gridRect.height / 2);
+
+                    // 2차 클램프
 
 
-            _camera.transform.position = new Vector3(x, y, Define.CAMERA_Z);
+                    _camera.transform.position = new Vector3(x, y, Define.CAMERA_Z);
+                }
+                // scroll
+                Vector2 scrollDelta = Mouse.current.scroll.ReadValue();
+
+                if (scrollDelta.y != 0)
+                {
+                    float scroll = scrollDelta.y * _scrollRatio;
+                    _camera.orthographicSize += _scrollFlip ? scroll : -scroll;
+                    // clamp
+                    _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize, _scrollClamp.x, _scrollClamp.y);
+                }
+                break;
+            default:
+                break;
         }
-        // scroll
-        Vector2 scrollDelta = Mouse.current.scroll.ReadValue();
-
-        if (scrollDelta.y != 0)
-        {
-            float scroll = scrollDelta.y * _scrollRatio;
-            _camera.orthographicSize += _scrollFlip ? scroll : -scroll;
-            // clamp
-            _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize, _scrollClamp.x, _scrollClamp.y);
-        }
-
     }
     #endregion
 
